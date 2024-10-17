1b35b5a674b97467cdb6eb319acbed13d3ae6943
Michael Collins <mfcollins3@me.com>
2024-10-16 22:30:33 -0700

    feat: install Dart Sass
    I created a new development container feature that will download and
    install Dart Sass into the development container.

---
+++ src/dart-sass/README.md
@@ -0,0 +1,31 @@
+# Dart Sass
+
+## About the Dart Sass Feature
+
+This feature installs [Dart Sass](https://sass-lang.com/dart-sass/) into a development container so that it can be used to generate CSS from Sass documents. This feature will download and install pre-built versions of Dart Sass from [GitHub Releases](https://github.com/sass/dart-sass/releases).
+
+## Usage
+
+To install Dart Sass into your development container, add it as a feature to your `devcontainer.json` file:
+
+```json
+{
+    "features": {
+        "ghcr.io/mfcollins3/devcontainers/dart-sass:0": {
+            "version": "latest"
+        }
+    }
+}
+```
+
+The `[options]` block indicates the optional presence of options that control the installation of Dart Sass. Currently, the only available option is `version`:
+
+- `version`: the version number of Dart Sass to be installed into the development container. If not specified or if specified as `latest`, the latest version of Dart Sass will be downloaded from GitHub and installed.
+
+## Development Container Effects
+
+This feature will download and install Dart Sass into the development container. The Dart Sass files are stored in the `/usr/local/dart-sass` directory. This path is also added to the `PATH` environment variable for the bash and zsh shells.
+
+Since Dart Sass is added to the `PATH`, you just need to run:
+
+    $ sass
---
+++ src/dart-sass/install.sh
@@ -0,0 +1,136 @@
+#!/usr/bin/env bash
+
+# Copyright 2024 Michael F. Collins, III
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to
+# deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNETION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+
+#-------------------------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
+#-------------------------------------------------------------------------------------------------------------
+
+set -e
+
+VERSION="${VERSION:-"latest"}"
+USERNAME="${USERNAME:-"${_REMOTE_USER:-"automatic"}"}"
+UPDATE_RC="${UPDATE_RC:-"true"}"
+DARTSASS_DIR="${DARTSASS_DIR:-"/usr/local"}"
+INSTALL_DIR="${DARTSASS_DIR}/dart-sass"
+
+# Clean up
+rm -rf /var/lib/apt/lists/*
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+    exit 1
+fi
+
+# Ensure that login shells get the correct path if the user updated the PATH using ENV.
+rm -f /etc/profile.d/00-restore-env.sh
+echo "export PATH=${PATH//$(sh -lc 'echo $PATH')/\$PATH}" > /etc/profile.d/00-restore-env.sh
+chmod +x /etc/profile.d/00-restore-env.sh
+
+if [ "${USERNAME}" = "auto" ] || [ "${USERNAME}" = "automatic" ]; then
+    USERNAME=""
+    POSSIBLE_USERS=("vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
+    for CURRENT_USER in "${POSSIBLE_USERS[@]}"; do
+        if id -u ${CURRENT_USER} > /dev/null 2>&1; then
+            USERNAME=${CURRENT_USER}
+            break
+        fi
+    done
+    if [ "${USERNAME}" = "" ]; then
+        USERNAME=root
+    fi
+elif [ "${USERNAME}" = "none" ] || ! id -u ${USERNAME} > /dev/null 2>&1; then
+    USERNAME=root
+fi
+
+architecture="$(uname -m)"
+if [ "${architecture}" != "amd64" ] && [ "${architecture}" != "x86_64" ] && [ "${architecture}" != "arm64" ] && [ "${architecture}" != "aarch64" ]; then
+    echo "(!) Architecture $architecture unsupported"
+    exit 1
+fi
+
+updaterc() {
+    if [ "${UPDATE_RC}" = "true" ]; then
+        echo "Updating /etc/bash.bashrc and /etc/zsh/zshrc..."
+        if [[ "$(cat /etc/bash.bashrc)" != *"$1"* ]]; then
+            echo -e "$1" >> /etc/bash.bashrc
+        fi
+        if [ -f "/etc/zsh/zshrc" ] && [[ "$(cat /etc/zsh/zshrc)" != *"$1"* ]]; then
+            echo -e "$1" >> /etc/zsh/zshrc
+        fi
+    fi
+}
+
+apt_get_update()
+{
+    if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+        echo "Running apt-get update..."
+        apt-get update -y
+    fi
+}
+
+# Checks if packages are installed and installs them if not
+check_packages() {
+    if ! dpkg -s "$@" > /dev/null 2>&1; then
+        apt_get_update
+        apt-get -y install --no-install-recommends "$@"
+    fi
+}
+
+if ! cat /etc/group | grep -e "^dartsass:" > /dev/null 2>&1; then
+    groupadd -r dartsass
+fi
+usermod -a -G dartsass "${USERNAME}"
+
+# Install dependencies
+check_packages curl ca-certificates tar
+
+if [ "${VERSION}" = "latest" ]; then
+    export VERSION=$(curl -s https://api.github.com/repos/sass/dart-sass/releases/latest | grep "tag_name" | awk '{print substr($2, 2, length($2)-3)}')
+fi
+
+mkdir -p "${DARTSASS_DIR}"
+
+if [ "$(uname -m)" == "aarch64" ]; then
+    arch="arm64"
+else
+    arch="x64"
+fi
+
+dartsass_filename="dart-sass-${VERSION}-linux-${arch}.tar.gz"
+dartsass_url="https://github.com/sass/dart-sass/releases/download/${VERSION}/${dartsass_filename}"
+echo "Downloading ${dartsass_url}..."
+curl -fsSLO --compressed "${dartsass_url}"
+tar -xzf "${dartsass_filename}" -C "${DARTSASS_DIR}"
+rm "$dartsass_filename"
+
+updaterc "export DARTSASS_DIR=${INSTALL_DIR}"
+updaterc "export PATH=\$PATH:\$DARTSASS_DIR"
+
+chown -R "${USERNAME}:dartsass" "${INSTALL_DIR}"
+chmod -R g+r+w "${INSTALL_DIR}"
+find "${INSTALL_DIR}" -type d -print0 | xargs -n 1 -0 chmod g+s
+
+# Clean up
+rm -rf /var/lib/apt/lists/*
+
+echo "Done!"
